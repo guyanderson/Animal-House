f6c32ce4fbd6fb0f4bc071349c87a174c189c406
guy <luckythelich@hotmail.com>
2017-07-07 10:56:58 -0700

    displaying 'Master animal list' through 'child animal list' going thorought 'list.component' from app.component.

---
+++ app/animal-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Animal } from './animal.model';
+
+
+
+@Component({
+  selector: 'animal-list',
+  template: `
+  <ul>
+
+    <li *ngFor="let currentAnimal of childAnimalList"><strong>Species:</strong> {{currentAnimal.species}}<br><strong>Name:</strong> {{currentAnimal.name}}<br><strong>Age:</strong> {{currentAnimal.age}}<br><strong>Diet:</strong>{{currentAnimal.diet}}<br><strong>Location:</strong> {{currentAnimal.location}}<br><strong>Number of Caretakers:</strong> {{currentAnimal.caretaker}}<br><strong>Sex:</strong> {{currentAnimal.sex}}<br><strong>Likes:</strong> {{currentAnimal.like}}<br><strong>dislike:</strong> {{currentAnimal.dislike}}<br><br><br></li>
+
+  </ul>
+  `
+})
+
+export class AnimalListComponent {
+    @Input() childAnimalList: Animal[];
+
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-import { Animal } from './animal.model'
+import { Animal } from './animal.model';
 
 @Component({
     selector: 'app-root',
     template: `
     <div class="container">
         <h1>Animal House</h1>
+        <br>
+        <br>
+        <animal-list [childAnimalList]="masterAnimalList"></animal-list>
     </div>
     `
 })
 
 export class AppComponent {
+
+masterAnimalList: Animal[] = [
+    new Animal('Arctic Fox','Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
+    new Animal('Ocelot','Prince', 4, 'Carnivore', 'Tropical Rain Forest Building', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based'),
+    new Animal('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 2, 'Female', 'Delicate roots and leaves', 'Loud Noises')
+];
+
 }
